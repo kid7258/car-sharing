3b47252e6211fcf446928a34d2d16f1ef621d009
kid7258 <kid7258@naver.com>
2020-11-19 23:01:50 +0900

    Project init

---
+++ src/test/java/study/carsharing/CarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package study.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
