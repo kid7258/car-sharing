e265c3b5e49a0bc4e0eebd364a48ca99830a8804
jinhyeon-park <jinhyeon-park@linecorp.com>
2020-12-09 22:33:51 +0900

    Add create ymdt and last mod ymdt column

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,2 +1,21 @@
-create table MEMBER(id bigint auto_increment, email varchar(255), name varchar(255), password varchar(255), role varchar(10));
-create table BOARD(id bigint auto_increment, member_id bigint, title varchar(255), content CLOB, created_date date, updated_date date);
+create table MEMBER
+(
+    id          bigint auto_increment,
+    email       varchar(255),
+    name        varchar(255),
+    password    varchar(255),
+    role        varchar(10),
+    create_ymdt timestamp,
+    last_mod_ymdt timestamp
+);
+
+create table BOARD
+(
+    id           bigint auto_increment,
+    member_id    bigint,
+    title        varchar(255),
+    content      CLOB,
+    create_ymdt timestamp,
+    last_mod_ymdt timestamp
+);
+
